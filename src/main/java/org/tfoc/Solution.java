02aad316b22f7981064f8ba32346c9c7be480f68
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-02-13 22:17:54 +0100

    feat: Brute force attack solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,9 +5,25 @@ package org.tfoc;
  */
 public class Solution {
 
-    public Integer maxSubArray(Integer[] nums) {
+    public static Integer maxSubArrayV1(Integer[] nums) {
+        //Brute force solution O(n^2)
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
 
-        return null;
+        for(int i = 0; i < nums.length; i++){
+
+            for(int j = i; j < nums.length; j++){
+
+                currentSum += nums[j];
+
+                if(currentSum > maxSum){
+                    maxSum = currentSum;
+                }
+            }
+            currentSum = 0;
+        }
+
+        return maxSum;
     }
 
 }
